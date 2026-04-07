86716026a1e8d27682145f3b0d2ba3a7f60a5849
devaraju03 <doyourownwork.03@gmail.com>
2026-04-07 16:47:51 +0530

    Add typing effect script for text display

---
+++ script.js
@@ -0,0 +1,13 @@
+/* script.js */
+const text = "Aspiring Software Developer | Web Developer";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 50);
+  }
+}
+
+window.onload = typing;
